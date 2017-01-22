b1e25fb1cbde4243e4fc0ede4c627ce37d27fb87
Joshua Molina <joshua.1994@hotmail.es>
2017-01-22 21:38:00 +0100

    recursos

--- Acierta_y_Corre/app/build/generated/source/r/debug/com/albertbc94gmail/acierta_y_corre/R.java
+++ Acierta_y_Corre/app/build/generated/source/r/debug/com/albertbc94gmail/acierta_y_corre/R.java
@@ -2054,6 +2054,15 @@ containing a value of this type.
         public static final int temas=0x7f060001;
     }
     public static final class string {
+        public static final int Alumbrado=0x7f070015;
+        public static final int EmpezarTest=0x7f070016;
+        public static final int Errores=0x7f070017;
+        public static final int Finalizar=0x7f070018;
+        public static final int Hist=0x7f070019;
+        public static final int Normas=0x7f07001a;
+        public static final int Señales=0x7f07001b;
+        public static final int Tema=0x7f07001c;
+        public static final int Velocidad=0x7f07001d;
         public static final int abc_action_bar_home_description=0x7f070000;
         public static final int abc_action_bar_home_description_format=0x7f070001;
         public static final int abc_action_bar_home_subtitle_description_format=0x7f070002;
@@ -2064,18 +2073,18 @@ containing a value of this type.
         public static final int abc_activitychooserview_choose_application=0x7f070007;
         public static final int abc_capital_off=0x7f070008;
         public static final int abc_capital_on=0x7f070009;
-        public static final int abc_font_family_body_1_material=0x7f070015;
-        public static final int abc_font_family_body_2_material=0x7f070016;
-        public static final int abc_font_family_button_material=0x7f070017;
-        public static final int abc_font_family_caption_material=0x7f070018;
-        public static final int abc_font_family_display_1_material=0x7f070019;
-        public static final int abc_font_family_display_2_material=0x7f07001a;
-        public static final int abc_font_family_display_3_material=0x7f07001b;
-        public static final int abc_font_family_display_4_material=0x7f07001c;
-        public static final int abc_font_family_headline_material=0x7f07001d;
-        public static final int abc_font_family_menu_material=0x7f07001e;
-        public static final int abc_font_family_subhead_material=0x7f07001f;
-        public static final int abc_font_family_title_material=0x7f070020;
+        public static final int abc_font_family_body_1_material=0x7f07001e;
+        public static final int abc_font_family_body_2_material=0x7f07001f;
+        public static final int abc_font_family_button_material=0x7f070020;
+        public static final int abc_font_family_caption_material=0x7f070021;
+        public static final int abc_font_family_display_1_material=0x7f070022;
+        public static final int abc_font_family_display_2_material=0x7f070023;
+        public static final int abc_font_family_display_3_material=0x7f070024;
+        public static final int abc_font_family_display_4_material=0x7f070025;
+        public static final int abc_font_family_headline_material=0x7f070026;
+        public static final int abc_font_family_menu_material=0x7f070027;
+        public static final int abc_font_family_subhead_material=0x7f070028;
+        public static final int abc_font_family_title_material=0x7f070029;
         public static final int abc_search_hint=0x7f07000a;
         public static final int abc_searchview_description_clear=0x7f07000b;
         public static final int abc_searchview_description_query=0x7f07000c;
@@ -2085,14 +2094,15 @@ containing a value of this type.
         public static final int abc_shareactionprovider_share_with=0x7f070010;
         public static final int abc_shareactionprovider_share_with_application=0x7f070011;
         public static final int abc_toolbar_collapse_description=0x7f070012;
-        public static final int app_name=0x7f070021;
-        public static final int error_message=0x7f070022;
-        public static final int num_errors=0x7f070023;
-        public static final int num_succes=0x7f070024;
+        public static final int app_name=0x7f07002a;
+        public static final int back=0x7f07002b;
+        public static final int error_message=0x7f07002c;
+        public static final int num_errors=0x7f07002d;
+        public static final int num_succes=0x7f07002e;
         public static final int search_menu_title=0x7f070013;
         public static final int status_bar_notification_info_overflow=0x7f070014;
-        public static final int succes_message=0x7f070025;
-        public static final int titolDialeg=0x7f070026;
+        public static final int succes_message=0x7f07002f;
+        public static final int titolDialeg=0x7f070030;
     }
     public static final class style {
         public static final int AlertDialog_AppCompat=0x7f09008a;
