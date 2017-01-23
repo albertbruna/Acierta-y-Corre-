801e9c01c8808edf42cf1eeaf0889169ffbe80df
Joshua Molina <joshua.1994@hotmail.es>
2017-01-23 18:30:22 +0100

    APK

---
+++ Acierta_y_Corre/app/build/generated/source/buildConfig/release/com/albertbc94gmail/acierta_y_corre/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.albertbc94gmail.acierta_y_corre;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.albertbc94gmail.acierta_y_corre";
+  public static final String BUILD_TYPE = "release";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
